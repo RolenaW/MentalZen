1c95c6df6ff9ea18ce63acd34df5d587d9f1a5ed
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-04-22 00:51:26 -0400

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // REQUIRED
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MentalZenApp());
 }
 class MentalZenApp extends StatelessWidget {
